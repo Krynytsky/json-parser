fd843d170b4bde84e5b1543aa5c7df39cb22bbec
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-12-14 00:32:22 +0200

    [krynytsky]#Created JsonParser class to read value from json file, also use to get child companies Map;

--- src/main/java/jsonparser/JsonParser.java
+++ src/main/java/jsonparser/JsonParser.java
@@ -7,29 +7,19 @@ import java.io.IOException;
 import java.util.Map;
 
 public class JsonParser {
-
-    public static void main(String[] args) throws IOException {
-        parseFromJson("/home/yurii/jsonParser/src/main/resources/json/task.json",
-                "/home/yurii/jsonParser/src/main/resources/json/notExistCompany.json");
-
-    }
-
-
-    public static void parseFromJson(String file, String file2) throws IOException {
+    /**
+     * @param file
+     * @return childCompaniesMap
+     * @throws IOException
+     */
+    public Map<String, Object> parseChildCompanyJson(String file) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Map<String, Object>> dataMap = mapper.readValue(new File(file), Map.class);
-        Map<String, Map<String, Object>> dataMap1 = mapper.readValue(new File(file2), Map.class);
-
-        Object o = dataMap.get("manufacturers").get("childCompanies");
-        Object o1 = dataMap1.get("manufacturers").get("childCompanies");
-
-        if (o.equals(o1)) {
-            System.out.println("isEquals");
-            System.out.println(o + "\n" + o1);
-        } else {
-            System.out.println("notEquals");
-            System.out.println(o + "\n" + o1);
+        Map<String, Map<String, Map<String, Object>>> childCompanyDataMap = mapper.readValue(new File(file), Map.class);
+        Map<String, Object> childCompaniesMap = childCompanyDataMap.get("manufacturers").get("childCompanies");
+        for (Map.Entry<String, Object> entry : childCompaniesMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
+        return childCompaniesMap;
     }
 }
 
